40045dfce9feca4aea6e7f99dabd07a8d46379db
Bobbi York <york1670@gmail.com>
2018-09-30 19:08:41 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,1 +1,19 @@
-# Write your code here.
+katz_deli = []
+
+def line(deli)
+  if deli == []
+    puts "The line is currently empty."
+  else
+    deli_line = []
+    deli.each.with_index(1) do |customer,index|
+        deli_line << " #{index}. #{customer}"
+    end
+    puts "The line is currently:" + deli_line.join
+  end
+end
+
+def take_a_number (deli, customer)
+  deli << customer
+  position_in_line = deli.length
+  puts "Welcome, #{customer}. You are number #{position_in_line} in line."
+end
