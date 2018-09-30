e791174540c56f4eae9b304fdc4f6eef3cf00a03
Bobbi York <york1670@gmail.com>
2018-09-30 19:33:22 +0000

    automatically backed up by learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,7 +21,7 @@ end
 def now_serving(deli)
   customer = deli.shift
     puts "Currently serving #{customer}."
-  if customer = 0
+  if customer < 0
       puts "There is nobody waiting to be served!"
   end
 end
